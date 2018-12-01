fd84bab8ed33a87677a03d114580eb3d3dc5fe32
Delphine <delphine.rousselet@devinci.fr>
2018-12-01 18:07:00 -0500

    add class Move

--- src/model/Move.java
+++ src/model/Move.java
@@ -1,28 +1,37 @@
 package model;
 
 public class Move {
-
-	private String startCase;
-	private String endCase;
-	private Chessman chessman;
-	private Chessboard chessboard;
-
-	public Move(String startCase, String endCase, Chessman chessman, Chessboard chessboard) {
-		this.startCase = startCase;
-		this.endCase = endCase;
-		this.chessman = chessman;
-		this.chessboard = chessboard;
+	// Attributes
+	private int start;
+	private int end;
+	private String promotion;
+	
+	// Constructor
+	public Move(int start, int end) {
+		super();
+		this.start = start;
+		this.end = end;
 	}
 	
+	// Getters and setters
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public String getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(String promotion) {
+		this.promotion = promotion;
+	}
 	
-	public String getStartCase() {return startCase;}
-	public void setStartCase(String startCase) {this.startCase = startCase;}
-	public String getEndCase() {return endCase;}
-	public void setEndCase(String endCase) {this.endCase = endCase;}
-	public Chessman getChessman() {return chessman;}
-	public void setChessman(Chessman chessman) {this.chessman = chessman;}
-	public Chessboard getChessboard() {	return chessboard;}
-	public void setChessboard(Chessboard chessboard) {this.chessboard = chessboard;}
-	
-	
+		
 }
